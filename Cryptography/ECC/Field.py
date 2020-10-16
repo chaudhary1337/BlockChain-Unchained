5f5e3b8c7acb2336985767365795373b75597f6c
smartypants042 <tanishqchaudhary511@gmail.com>
2020-10-16 16:24:05 +0530

    Reflecting Minor Changes

--- Cryptography/ECC/Field.py
+++ Cryptography/ECC/Field.py
@@ -4,7 +4,7 @@ class FieldElement():
     """
 
     def __init__(self, value, prime, use_defaults=True):
-        
+        self.use_defaults = use_defaults
         # Handling prime numbers for the field
         if use_defaults:
             self.prime = prime
@@ -47,16 +47,19 @@ class FieldElement():
     def __add__(self, other):
         try:
             if self.prime == other.prime:
-                return self.__class__((self.value + other.value)%self.prime, self.prime)
+                return self.__class__((self.value + other.value)%self.prime, self.prime, self.use_defaults)
             else:
                 raise TypeError(f"Numbers belong to different fields {self.prime} and {other.prime}")
         except:
             raise TypeError("Not possible to Add")
-    
+
+    def __radd__(self, num):
+        return self.__class__((self.value+num)%self.prime, self.prime, self.use_defaults)
+
     def __sub__(self, other):
         try:
             if self.prime == other.prime:
-                return self.__class__((self.value - other.value)%self.prime, self.prime)
+                return self.__class__((self.value - other.value)%self.prime, self.prime, self.use_defaults)
             else:
                 raise TypeError(f"Numbers belong to different fields {self.prime} and {other.prime}")
         except:
@@ -65,16 +68,19 @@ class FieldElement():
     def __mul__(self, other):
         try:
             if self.prime == other.prime:
-                return self.__class__((self.value * other.value)%self.prime, self.prime)
+                return self.__class__((self.value * other.value)%self.prime, self.prime, self.use_defaults)
             else:
                 raise TypeError(f"Numbers belong to different fields {self.prime} and {other.prime}")
         except:
             raise TypeError("Not possible to Multiply")
 
+    def __rmul__(self, coefficient):
+        return self.__class__((self.value * coefficient)%self.prime, self.prime, self.use_defaults)
+
     def __pow__(self, other):
         try:
             other = other % (self.prime - 1)
-            return self.__class__(pow(self.value, other, self.prime), self.prime)
+            return self.__class__(pow(self.value, other, self.prime), self.prime, self.use_defaults)
         except:
             raise ValueError("Not possible to raise the Power")
     
@@ -88,11 +94,16 @@ class FieldElement():
             raise ValueError("Not able to access power function")
 
 if __name__ == "__main__":
-    a = FieldElement(5, 23)
-    b = FieldElement(3, 23)
-    print("5+3\t:", a+b)
-    print("3-5\t:", b-a)
-    print("5*3\t:", a*b)
-    print("5/3\t:", a/b)
-    print("5**3\t:", a**3)
-    print("5**3*3\t:", a**3 * b)
+    # a = FieldElement(5, 23)
+    # b = FieldElement(3, 23)
+    # print("5+3\t:", a+b)
+    # print("3-5\t:", b-a)
+    # print("5*3\t:", a*b)
+    # print("5/3\t:", a/b)
+    # print("5**3\t:", a**3)
+    # print("5**3*3\t:", a**3 * b)
+
+    x = FieldElement(0, 223, False)
+    y = FieldElement(4, 223, False)
+    z = 5 
+    print(z+y)
--- Cryptography/ECC/Point.py
+++ Cryptography/ECC/Point.py
@@ -1,7 +1,7 @@
 from Field import FieldElement
 
 class ECPoint():
-    def __init__(self, x=None, y=None, a=None, b=None, p=None, use_defaults=True, internal=False):    
+    def __init__(self, x=None, y=None, a=None, b=None, p=None, use_defaults=True):    
         """
         Using defaults will override the provided value(s) is any.
         """
@@ -10,20 +10,12 @@ class ECPoint():
         # Handling the Elliptic Curve
         if use_defaults:
             self.p = (2**256 - 2**32 - 2**9 - 2**8 - 2**7 - 2**6 - 2**4 - 1)
-            if not internal:
-                self.a = FieldElement(0, self.p, self.use_defaults)
-                self.b = FieldElement(7, self.p, self.use_defaults)
-            else:
-                self.a = a
-                self.b = b
+            self.a = 0
+            self.b = 7
         else:
             self.p = p
-            if not internal:
-                self.a = FieldElement(a, self.p, self.use_defaults)
-                self.b = FieldElement(b, self.p, self.use_defaults)
-            else:
-                self.a = a
-                self.b = b
+            self.a = a
+            self.b = b
 
         # Checking for Curve Validity
         if not self.valid_curve():
@@ -34,34 +26,23 @@ class ECPoint():
         if x == 'inf' and y == 'inf':
             self.x = None
             self.y = None
+        elif x and y:
+            self.x = x
+            self.y = y
         else:
-            try:
-                if not internal:
-                    self.x = FieldElement(x, self.p, self.use_defaults)
-                    self.y = FieldElement(y, self.p, self.use_defaults)
-                else:
-                    self.x = x
-                    self.y = y
-            except:
-                raise ValueError("x and y must be both valid or both 'inf'")
+            raise ValueError("x and y must be both valid or both 'inf'")
 
         if not self.point_on_curve():
             raise ValueError("Point Specified must be on the Curve")
             
     def valid_curve(self):
-        return bool(
-            FieldElement(4, self.p, self.use_defaults) * self.a**3 + 
-            FieldElement(27%self.p, self.p, self.use_defaults) * self.b**2
-        )
+        return bool(4 * self.a**3 + 27 * self.b**2)
     
     def point_on_curve(self):
         if self.x == None and self.y == None:
             return True
-        else:    
-            return (
-                self.y**2 == 
-                self.x**3 + self.a*self.x + self.b
-            )
+        else:
+            return (self.y**2 == self.b + self.a*self.x + self.x**3)
     
     def __add__(self, other):
         try:
@@ -84,23 +65,21 @@ class ECPoint():
             m = (self.y-other.y)/(self.x-other.x)
         else:
             if self.y:
-                m = (FieldElement(3, self.p, self.use_defaults)*self.x**2 + self.a)/(FieldElement(2, self.p, self.use_defaults)*self.y)
+                m = (3*self.x**2 + self.a)/(2*self.y)
             else:
-                return self.__class__('inf', 'inf', self.a, self.b, self.p, self.use_defaults, internal=True)
+                return self.__class__('inf', 'inf', self.a, self.b, self.p, self.use_defaults)
 
         x = m**2 - self.x - other.x
         y = m*(self.x - x) - self.y
-        return self.__class__(x, y, self.a, self.b, self.p, self.use_defaults, internal=True)
+        print(f"SLOPE: {m}, x: {x}, y: {y}")
+        return self.__class__(x, y, self.a, self.b, self.p, self.use_defaults)
 
 
     def __mul__(self, other):
         pass
 
-point1 = ECPoint('inf', 'inf', use_defaults=False, a=0, b=0, p=107)
-point2 = ECPoint(x=4, y=8, use_defaults=False, a=0, b=0, p=107)
-point3 = ECPoint(x=9, y=27, use_defaults=False, a=0, b=0, p=107)
-point4= ECPoint(x=9, y=27, use_defaults=False, a=0, b=0, p=97)
-# print(type(FieldElement(5, 7, False)) == type(FieldElement(0, 1, False)))
+point1 = ECPoint('inf', 'inf', use_defaults=False, a=0, b=7, p=223)
+point2 = ECPoint(x=FieldElement(47, 223, False), y=FieldElement(71, 223, False), use_defaults=False, a=0, b=7, p=223)
+point3 = ECPoint(x=FieldElement(17, 223, False), y=FieldElement(56, 223, False), use_defaults=False, a=0, b=7, p=223)
 
-point5 = point2+point2
-print(point5.x);print(point5.y)
+point4 = point2+point3
