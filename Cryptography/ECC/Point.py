659bc8fa3059e97bd75af77e4f994e45db64576e
smartypants042 <tanishqchaudhary511@gmail.com>
2020-10-16 08:44:06 +0530

    Removing FieldElements for Convenience

--- Cryptography/ECC/Point.py
+++ Cryptography/ECC/Point.py
@@ -1,7 +1,7 @@
 from Field import FieldElement
 
 class ECPoint():
-    def __init__(self, x=None, y=None, a=None, b=None, p=None, use_defaults=True, internal=False):    
+    def __init__(self, x=None, y=None, a=None, b=None, p=None, use_defaults=True):    
         """
         Using defaults will override the provided value(s) is any.
         """
@@ -10,20 +10,20 @@ class ECPoint():
         # Handling the Elliptic Curve
         if use_defaults:
             self.p = (2**256 - 2**32 - 2**9 - 2**8 - 2**7 - 2**6 - 2**4 - 1)
-            if not internal:
-                self.a = FieldElement(0, self.p, self.use_defaults)
-                self.b = FieldElement(7, self.p, self.use_defaults)
-            else:
-                self.a = a
-                self.b = b
+            # if not internal:
+            #     self.a = FieldElement(0, self.p, self.use_defaults)
+            #     self.b = FieldElement(7, self.p, self.use_defaults)
+            # else:
+            self.a = 0
+            self.b = 7
         else:
             self.p = p
-            if not internal:
-                self.a = FieldElement(a, self.p, self.use_defaults)
-                self.b = FieldElement(b, self.p, self.use_defaults)
-            else:
-                self.a = a
-                self.b = b
+            # if not internal:
+            #     self.a = FieldElement(a, self.p, self.use_defaults)
+            #     self.b = FieldElement(b, self.p, self.use_defaults)
+            # else:
+            self.a = a
+            self.b = b
 
         # Checking for Curve Validity
         if not self.valid_curve():
@@ -36,12 +36,12 @@ class ECPoint():
             self.y = None
         else:
             try:
-                if not internal:
-                    self.x = FieldElement(x, self.p, self.use_defaults)
-                    self.y = FieldElement(y, self.p, self.use_defaults)
-                else:
-                    self.x = x
-                    self.y = y
+                # if not internal:
+                #     self.x = FieldElement(x, self.p, self.use_defaults)
+                #     self.y = FieldElement(y, self.p, self.use_defaults)
+                # else:
+                self.x = x
+                self.y = y
             except:
                 raise ValueError("x and y must be both valid or both 'inf'")
 
@@ -49,10 +49,11 @@ class ECPoint():
             raise ValueError("Point Specified must be on the Curve")
             
     def valid_curve(self):
-        return bool(
-            FieldElement(4, self.p, self.use_defaults) * self.a**3 + 
-            FieldElement(27%self.p, self.p, self.use_defaults) * self.b**2
-        )
+        # return bool(
+        #     FieldElement(4, self.p, self.use_defaults) * self.a**3 + 
+        #     FieldElement(27%self.p, self.p, self.use_defaults) * self.b**2
+        # )
+        return bool(4*self.a**3 + 27*self.b**2)
     
     def point_on_curve(self):
         if self.x == None and self.y == None:
@@ -84,23 +85,25 @@ class ECPoint():
             m = (self.y-other.y)/(self.x-other.x)
         else:
             if self.y:
-                m = (FieldElement(3, self.p, self.use_defaults)*self.x**2 + self.a)/(FieldElement(2, self.p, self.use_defaults)*self.y)
+                m = (3*self.x**2 + self.a)/(2*self.y)
             else:
-                return self.__class__('inf', 'inf', self.a, self.b, self.p, self.use_defaults, internal=True)
+                return self.__class__('inf', 'inf', self.a, self.b, self.p, self.use_defaults)
 
         x = m**2 - self.x - other.x
         y = m*(self.x - x) - self.y
-        return self.__class__(x, y, self.a, self.b, self.p, self.use_defaults, internal=True)
+        return self.__class__(x, y, self.a, self.b, self.p, self.use_defaults)
 
 
     def __mul__(self, other):
         pass
 
-point1 = ECPoint('inf', 'inf', use_defaults=False, a=0, b=0, p=107)
-point2 = ECPoint(x=4, y=8, use_defaults=False, a=0, b=0, p=107)
-point3 = ECPoint(x=9, y=27, use_defaults=False, a=0, b=0, p=107)
-point4= ECPoint(x=9, y=27, use_defaults=False, a=0, b=0, p=97)
+point1 = ECPoint('inf', 'inf', use_defaults=False, a=0, b=8, p=107)
+point2 = ECPoint(x=2, y=4, use_defaults=False, a=0, b=8, p=107)
+point3 = ECPoint(x=-2, y=0, use_defaults=False, a=0, b=8, p=107)
+point4= ECPoint(x=2, y=4, use_defaults=False, a=0, b=8, p=97)
 # print(type(FieldElement(5, 7, False)) == type(FieldElement(0, 1, False)))
 
-point5 = point2+point2
-print(point5.x);print(point5.y)
+# temp = point2
+# for i in range(10):
+#     temp += point2
+#     # print(temp.x, temp.y)
