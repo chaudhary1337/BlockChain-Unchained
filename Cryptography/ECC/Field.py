3fc1b85897093c5140ef4dcfc2dac50a9553ddbc
smartypants042 <tanishqchaudhary511@gmail.com>
2020-10-16 16:43:10 +0530

    Simplyfing The FieldElement Class

--- Cryptography/ECC/Field.py
+++ Cryptography/ECC/Field.py
@@ -3,37 +3,13 @@ class FieldElement():
     A class for converting vanilla elements to field elements
     """
 
-    def __init__(self, value, prime, use_defaults=True):
-        self.use_defaults = use_defaults
-        # Handling prime numbers for the field
-        if use_defaults:
-            self.prime = prime
-        else:
-            if self.check_prime(prime):
-                self.prime = prime
-            else:
-                raise ValueError(f"The number {prime} is not a prime.")
-
+    def __init__(self, value, prime):
+        self.prime = prime
         # Handling Value range
         if 0 <= value < prime:
             self.value = value
         else:
             raise ValueError(f"The number {value} must be between 0 and {prime}.")
-
-    def check_prime(self, num):
-        """
-        checks the input number is prime or not
-
-        Time Complexity: O(sqrt(N))
-        """
-        if num == None:
-            return False
-        i = 2
-        while i*i <= num:
-            if not(num % i):
-                return False
-            i += 1
-        return True
 
     def __eq__(self, other):
         try:
@@ -47,19 +23,19 @@ class FieldElement():
     def __add__(self, other):
         try:
             if self.prime == other.prime:
-                return self.__class__((self.value + other.value)%self.prime, self.prime, self.use_defaults)
+                return self.__class__((self.value + other.value)%self.prime, self.prime)
             else:
                 raise TypeError(f"Numbers belong to different fields {self.prime} and {other.prime}")
         except:
             raise TypeError("Not possible to Add")
 
     def __radd__(self, num):
-        return self.__class__((self.value+num)%self.prime, self.prime, self.use_defaults)
+        return self.__class__((self.value+num)%self.prime, self.prime)
 
     def __sub__(self, other):
         try:
             if self.prime == other.prime:
-                return self.__class__((self.value - other.value)%self.prime, self.prime, self.use_defaults)
+                return self.__class__((self.value - other.value)%self.prime, self.prime)
             else:
                 raise TypeError(f"Numbers belong to different fields {self.prime} and {other.prime}")
         except:
@@ -68,19 +44,19 @@ class FieldElement():
     def __mul__(self, other):
         try:
             if self.prime == other.prime:
-                return self.__class__((self.value * other.value)%self.prime, self.prime, self.use_defaults)
+                return self.__class__((self.value * other.value)%self.prime, self.prime)
             else:
                 raise TypeError(f"Numbers belong to different fields {self.prime} and {other.prime}")
         except:
             raise TypeError("Not possible to Multiply")
 
     def __rmul__(self, coefficient):
-        return self.__class__((self.value * coefficient)%self.prime, self.prime, self.use_defaults)
+        return self.__class__((self.value * coefficient)%self.prime, self.prime)
 
     def __pow__(self, other):
         try:
             other = other % (self.prime - 1)
-            return self.__class__(pow(self.value, other, self.prime), self.prime, self.use_defaults)
+            return self.__class__(pow(self.value, other, self.prime), self.prime)
         except:
             raise ValueError("Not possible to raise the Power")
     
@@ -92,18 +68,3 @@ class FieldElement():
             return self.__pow__(-other)
         except:
             raise ValueError("Not able to access power function")
-
-if __name__ == "__main__":
-    # a = FieldElement(5, 23)
-    # b = FieldElement(3, 23)
-    # print("5+3\t:", a+b)
-    # print("3-5\t:", b-a)
-    # print("5*3\t:", a*b)
-    # print("5/3\t:", a/b)
-    # print("5**3\t:", a**3)
-    # print("5**3*3\t:", a**3 * b)
-
-    x = FieldElement(0, 223, False)
-    y = FieldElement(4, 223, False)
-    z = 5 
-    print(z+y)
