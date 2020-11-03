e40bbf406bfc991bfa202ab70b12cebfc20da899
smartypants042 <tanishqchaudhary511@gmail.com>
2020-11-03 15:38:52 +0530

    Adding Math Functions for RSA

---
+++ Cryptography/RSA/math.py
@@ -0,0 +1,61 @@
+import random
+
+def generate_prime():
+    """
+    TODO
+    """
+    pass
+
+def get_totient(p, q):
+    return (p-1)*(q-1)
+
+def gcd(x, y):
+    if y == x:
+        return x
+
+    if x > y:
+        return gcd(x-y, y)
+    
+    return gcd(x, y-x)
+
+def is_prime(n, k=8):
+    """
+    uses the miller rabin test. based on probabilities.
+    worst case error bound: 4^(-k)
+
+    source: https://en.wikipedia.org/wiki/Miller%E2%80%93Rabin_primality_test
+    """
+    if not (n > 3) or not (n & 1):
+        return False
+    
+    # setup for miller rabin
+    temp = n-1
+    r = 0
+    while not (temp & 1):
+        temp >>= 1
+        r += 1
+    d = temp
+
+    for _ in range(k):
+        a = random.randrange(2, n-2)
+        x = pow(a, d, n)
+
+        if x == 1 or x == n-1:
+            continue
+
+        flag = False
+        for __ in range(r-1):
+            x = pow(x, 2, n)
+
+            if x == n-1:
+                flag = True
+                break
+        if flag:
+            continue
+
+        return False
+    
+    return True 
+
+def mod_inv(x, mod):
+    return pow(x, mod-2, mod)
