c1e3b847e5f776d1768ed5430c8edf2cc1962f2f
smartypants042 <tanishqchaudhary511@gmail.com>
2020-11-05 09:12:16 +0530

    Adding Person Class for Encryption Decryption

---
+++ Cryptography/RSA/Person.py
@@ -0,0 +1,18 @@
+from RSA import get_keys
+
+class Person():
+    def __init__(self, name, debug=True):
+        self.name = name
+        if debug:
+            print(f"Generating Key Pair for {self.name} ... ", end="")
+        
+        self.public_key, self.private_key = get_keys()
+        
+        if debug:
+            print(f"Done!")
+
+    def encrypt(self, other_public_key, message):
+        return pow(message, other_public_key['e'], other_public_key['n'])
+    
+    def decrypt(self, encrypted_message):
+        return pow(encrypted_message, self.private_key['d'], self.public_key['n'])
