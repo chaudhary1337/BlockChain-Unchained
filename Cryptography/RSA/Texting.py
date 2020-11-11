0a32ceb1a3179f404f4daed488e6c60160a646ba
smartypants042 <tanishqchaudhary511@gmail.com>
2020-11-11 08:09:23 +0530

    Reflecting Changes

--- Cryptography/RSA/Texting.py
+++ Cryptography/RSA/Texting.py
@@ -1,23 +1,49 @@
 import json
 from Person import Person
 
-# A = Person(name="Apples")
-# B = Person(name="Bananas")
+def make_people_pair(save=True):
+    """
+    Generates two peeps with each having valid keys.
+    """
+    global A
+    global B
+    
+    A = Person()
+    B = Person()
 
-# A_dump = json.dumps([A.public_key, A.private_key], indent=4)
-# B_dump = json.dumps([B.public_key, B.private_key], indent=4)
+    A_dump = json.dumps({'public_key': A.public_key, 'private_key': A.private_key}, indent=4)
+    B_dump = json.dumps({'public_key': B.public_key, 'private_key': B.private_key}, indent=4)
 
-# with open('A_test_subject.json', 'w') as f:
-#     f.write(A_dump)
+    if save:
+        with open('test_subject1.json', 'w') as f:
+            f.write(A_dump)
 
-# with open('B_test_subject.json', 'w') as f:
-#     f.write(B_dump)
+        with open('test_subject2.json', 'w') as f:
+            f.write(B_dump)
 
-# commons = {
-#     "A_public_key": A.public_key,
-#     "B_public_key": B.public_key
-# }
+def import_people():
+    global A
+    global B
+    
+    with open('test_subject1.json', 'r') as f:
+        A_data = json.load(f)
+    
+    with open('test_subject2.json', 'r') as f:
+        B_data = json.load(f)
 
-# encrypted_message = A.encrypt(commons['B_public_key'], message=42)
-# B.decrypt(encrypted_message)
-# print(pow(42, B.public_key['e']*B.private_key['d'], B.public_key['n']))
+    A = Person(generate=False, debug=False, data=A_data)
+    B = Person(generate=False, debug=False, data=B_data)
+
+def text():
+    import_people()
+
+    commons = {
+        "A_public_key": A.public_key,
+        "B_public_key": B.public_key
+    }
+
+    encrypted_message = A.encrypt(commons['B_public_key'], message=42)
+    print(encrypted_message)
+    print(B.decrypt(encrypted_message))
+
+text()
