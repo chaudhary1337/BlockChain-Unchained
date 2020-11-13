ecb245f9818d1b8502547838c3de2731a918b1fb
smartypants042 <tanishqchaudhary511@gmail.com>
2020-11-13 07:59:18 +0530

    Fixing All issues. Encryption/Decryption Test Working

--- Cryptography/RSA/RSA.py
+++ Cryptography/RSA/RSA.py
@@ -30,10 +30,10 @@ def get_keys(debug=False):
     if debug: print("Done.")
 
     # step 5: get mod inv of e, wrt totient; the private key exponent
-    # NOTE: The algorithm says to use totient, but it does not work.
-    # only on using n, we get a correct response.
+    # # NOTE: The algorithm says to use totient, but it does not work.
+    # # only on using n, we get a correct response.
     if debug: print("getting private key ...", end="")
-    d = mod_inv(e, n)
+    d = mod_inv(e, totient)
     if debug: print("Done.")
 
     return ({'n': n, 'e': e}, {'p':p, 'q':q, 'd':d})
@@ -41,12 +41,22 @@ def get_keys(debug=False):
 if __name__ == "__main__":
     public_key, private_key = get_keys(debug=True)
     
+    m = 55
     x = public_key['e']
     y = private_key['d']
-    m = public_key['n']
+    mod = get_totient(private_key['p'], private_key['q'])
+    n = public_key['n']
 
     print("Public Key: ", x)
     print("Private Key: ", y)
     print("Mod: ", m)
-    if x*y % m == 1:
-        print("CORRECT!")
+    if x*y % mod == 1:
+        print("INVERSE CORRECT!")
+    
+    encrypted = pow(55, x, n)
+    print(encrypted)
+    decrypted = pow(encrypted, y, n)
+    print(decrypted)
+
+    if decrypted == m:
+        print("ENCRYPTION/DECRYPTION CORRECT")
