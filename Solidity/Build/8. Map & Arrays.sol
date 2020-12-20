4f2c4a96cc404f4e45611e69c13f53652e0cf630
smartypants042 <tanishqchaudhary511@gmail.com>
2020-12-20 13:09:46 +0530

    Solidity: Build: Adding Mapping and Array-like Syntax

---
+++ Solidity/Build/8. Map & Arrays.sol
@@ -0,0 +1,50 @@
+// SPDX-License-Identifier: MIT
+// Taken directly form https://solidity-by-example.org/0.6/mapping/
+pragma solidity ^0.7.0;
+
+// mapping in solidity is equivalent to
+// dictionary in python3
+
+contract Mapping {
+    // Mapping from address to uint
+    mapping(address => uint256) public myMap;
+
+    function get(address _addr) public view returns (uint256) {
+        // Mapping always returns a value.
+        // If the value was never set, it will return the default value.
+        return myMap[_addr];
+    }
+
+    function set(address _addr, uint256 _i) public {
+        // Update the value at this address
+        myMap[_addr] = _i;
+    }
+
+    function remove(address _addr) public {
+        // Reset the value to the default value.
+        delete myMap[_addr];
+    }
+}
+
+contract NestedMapping {
+    // Nested mapping (mapping from address to another mapping)
+    mapping(address => mapping(uint256 => bool)) public nested;
+
+    function get(address _addr1, uint256 _i) public view returns (bool) {
+        // You can get values from a nested mapping
+        // even when it is not initialized
+        return nested[_addr1][_i];
+    }
+
+    function set(
+        address _addr1,
+        uint256 _i,
+        bool _boo
+    ) public {
+        nested[_addr1][_i] = _boo;
+    }
+
+    function remove(address _addr1, uint256 _i) public {
+        delete nested[_addr1][_i];
+    }
+}
