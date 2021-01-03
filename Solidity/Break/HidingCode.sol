1a2f123de853197aa8ccd589ed24c58f45ad1227
Tanishq Chaudhary <51963164+SmartyPants042@users.noreply.github.com>
2021-01-02 23:51:27 -0500

    Solidity Break: Updating HidingCode.sol

--- Solidity/Break/HidingCode.sol
+++ Solidity/Break/HidingCode.sol
@@ -3,7 +3,11 @@ pragma solidity ^0.7.0;
 
 /*
 Create a contract which looks the same, in this case is Mal.
-Should have the same function signatures and same variable sizes
+Should necessarily have the same function signatures. 
+Honestly, I am still confused over whether we need to have same variable sizes, and ordering.
+I found some cases of wrong ordering breaking code in case of instantiation (non)/intentnded;
+but skipping over variables works just fine.
+
 for the function that is being called 
 from Foo, to Bar (but actually to Mal).
 */
